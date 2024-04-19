7d5aae9b8fc844cbd3301f3a22781d935f8e169a
JUNGHOON AN <gnstm831@naver.com>
2024-04-19 15:13:10 +0900

    cache clear

--- TeamProject/src/main/java/team/cheesemate/HomeController.java
+++ TeamProject/src/main/java/team/cheesemate/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        System.out.println("hello!!");
+        System.out.println("hello!! 또 테스트");
         return "index";
 
 //        System.out.println("정훈 테스트");
