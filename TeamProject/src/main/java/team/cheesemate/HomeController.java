4e84fa434dc126b706728945f610e74e2fe91bd0
HeonyJ <jai0710@naver.com>
2024-04-19 14:42:06 +0900

    재헌 테스트

--- TeamProject/src/main/java/team/cheesemate/HomeController.java
+++ TeamProject/src/main/java/team/cheesemate/HomeController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+        System.out.println("재헌s 브랜치 테스트");
         return "index";
-        System.out.println("재헌 테스트 수정");
-
     }
 }
