eab7b0e2ab92dc6451d325166dd3c37e40b4e5b0
jjh9912 <jhy54081@gmail.com>
2024-05-14 14:03:17 +0900

    add interceptor

---
+++ src/main/java/team/cheese/controller/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,48 @@
+package team.cheese.controller.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        System.out.println("LoginCheckInterceptor - preHandle 호출되었습니다.");
+
+        // *** 로그인을 한 상태인지 체크한다 ***
+        // 1. 로그인 상태 체크
+        //      1.1 세션에 로그인 정보가 있는 경우 true 리턴
+        //      1.2 세션에 로그인 정보가 없는 경우 false 리턴
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute("userId") == null) {
+            System.out.println("요청한 화면 -> " + request.getRequestURI());
+
+            response.sendRedirect("loginForm" + "?from=" + request.getRequestURI());
+
+            System.out.println("로그인 정보가 없습니다.");
+            return false;
+        }
+        else {
+            String userId = session.getAttribute("userId").toString();
+            System.out.println("로그인 정보가 있습니다. -> " + userId);
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("LoginCheckInterceptor - postHandle 호출되었습니다.");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("LoginCheckInterceptor - afterHandle 호출되었습니다.");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/board_2.jsp
@@ -0,0 +1,35 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+
+<head>
+    <title>Board_2</title>
+</head>
+<body>
+
+<h1>*** BOARD_2 ***</h1>
+<h1>Board_2</h1>
+<h1>Board_2</h1>
+<h1>Board_2</h1>
+
+<c:if test="${sessionScope.userId != null}">
+    <p>일반 유저 입니다.</p>
+    <p>아이디 : ${sessionScope.userId}</p>
+    <p>닉네임 : ${sessionScope.userNick}</p>
+    <ul>
+        <c:forEach var="addr_cd" items="${sessionScope.userAddrCdDtoList}">
+            <li>${addr_cd.no}, ${addr_cd.addr_cd}</li>
+        </c:forEach>
+    </ul>
+</c:if>
+
+<c:if test="${sessionScope.adminId != null}">
+    <p>관리자 입니다.</p>
+    <p>아이디 : ${sessionScope.adminId}</p>
+</c:if>
+
+<form action="/home" method="get">
+    <button name="homeBnt">Home</button>
+</form>
+
+</body>
+</html>
