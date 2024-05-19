264ad3b92bc2cb02e785e381fff0cd986c22845d
ghkdwjdgk123 <ghkdwjdgk123@gmail.com>
2024-05-19 17:25:42 +0900

    before merge

--- src/main/java/team/cheese/controller/Event/ManageController.java
+++ src/main/java/team/cheese/controller/Event/ManageController.java
@@ -1,4 +1,4 @@
-package team.cheese.controller.event;
+package team.cheese.controller.Event;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
